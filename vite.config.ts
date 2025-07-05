8fec3fa57963bb87d64e73028cc5071b47c351ca
Samual <me@samual.uk>
2025-07-05 23:37:29 +0100

    build as single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from "vite"
+import type { UserConfig } from "vite"
+import { viteSingleFile } from "vite-plugin-singlefile"
 import solidPlugin from "vite-plugin-solid"
 
-export default defineConfig({ plugins: [ solidPlugin() ], server: { port: 3000 }, build: { target: "esnext" } })
+export default {
+	plugins: [ solidPlugin(), viteSingleFile() ],
+	server: { port: 3000 },
+	build: { target: "esnext" }
+} satisfies UserConfig
